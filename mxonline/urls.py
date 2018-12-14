49776da1d234fbf610175ad709c73a66dc5af3f9
one <onerf@sina.com>
2018-12-14 18:28:45 +0800

    set url for login.html

--- mxonline/urls.py
+++ mxonline/urls.py
@@ -19,5 +19,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='index')
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
 ]
