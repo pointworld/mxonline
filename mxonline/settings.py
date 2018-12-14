4f4f1c71a7eae8af63e4a24b910437be167123b5
one <onerf@sina.com>
2018-12-14 10:14:07 +0800

    alter language_code, time_zone and use_tz in settings.py

--- mxonline/settings.py
+++ mxonline/settings.py
@@ -120,15 +120,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# 语言默认是 en-us，这里改为中文
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+# 时区默认是 UTC，这里改为上海
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# 数据库存储使用时间，设置为 True 的话，时间会被存为 UTC 的时间
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
