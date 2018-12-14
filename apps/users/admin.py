1e05ad14efab37063c003b33a719f15b997e708b
one <onerf@sina.com>
2018-12-14 10:28:25 +0800

    register user_profile to admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import UserProfile
+
 # Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
