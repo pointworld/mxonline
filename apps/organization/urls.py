d8e461cba754ffe328b86ad55e925acff293274e
one <onerf@sina.com>
2018-12-17 23:32:17 +0800

    feat: index nav jumping

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     re_path('home/(?P<org_id>\d+)/', OrgHomeView.as_view(), name='org_home'),
     re_path('course/(?P<org_id>\d+)/', OrgCourseView.as_view(), name='org_course'),
     re_path('desc/(?P<org_id>\d+)/', OrgDescView.as_view(), name='org_desc'),
-    re_path('teacher/(?P<org_id>\d+)/', OrgTeacherView.as_view(), name='org_teacher'),
+    re_path('org/teacher/(?P<org_id>\d+)/', OrgTeacherView.as_view(), name='org_teacher'),
     # 机构收藏
     path('add_fav/', AddFavView.as_view(), name='add_fav'),
 
